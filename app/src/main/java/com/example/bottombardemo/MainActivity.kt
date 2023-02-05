db2c1c9bc591d4edf153eaf9762c03f81ef79727
balay <dlolhd@gmail.com>
2023-02-05 11:01:28 +0100

    Creating the navigation controller and host.

--- app/src/main/java/com/example/bottombardemo/MainActivity.kt
+++ app/src/main/java/com/example/bottombardemo/MainActivity.kt
@@ -10,6 +10,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.bottombardemo.screens.Contacts
+import com.example.bottombardemo.screens.Favorites
+import com.example.bottombardemo.screens.Home
 import com.example.bottombardemo.ui.theme.BottomBarDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +38,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
+    val navController = rememberNavController()
+}
 
+@Composable
+fun NavigationHost(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = NavRoutes.Home.route
+    ) {
+        composable(NavRoutes.Home.route) {
+            Home()
+        }
+        composable(NavRoutes.Contacts.route) {
+            Contacts()
+        }
+        composable(NavRoutes.Favorites.route) {
+            Favorites()
+        }
+    }
 }
 
 @Preview(showBackground = true)
@@ -41,3 +66,19 @@ fun DefaultPreview() {
         MainScreen()
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
